b0b1d7e3e02b6e6f28c0e322f54a0c275c7f60ca
Ufuk Bakan <ubakan@altayer.com>
2024-10-21 18:52:04 +0300

    refactor: rename file

--- src/plugins/backend/append-year.js
+++ src/plugins/backend/append-year.js
@@ -5,7 +5,7 @@ export default function addSignature({ html }) {
     const currentYear = new Date().getFullYear();
 
     // Find the footer element and append the year
-    $('footer').append(`<center>${currentYear}</center>`);
+    $('footer').append(`<center style="margin-top:1rem;">${currentYear}</center>`);
     
     // Get the updated HTML
     return $.html();
