0b55f915b1004dd79f8a595166290cd42736fbf2
Ufuk Bakan <ubakan@altayer.com>
2024-10-22 01:23:00 +0300

    feat: build command

---
+++ build.js
@@ -0,0 +1,30 @@
+import axios from 'axios';
+import { copyFolder } from 'copy-folder-util';
+import { mkdirSync, rmSync, writeFileSync } from 'fs';
+import { dirname, join } from 'path';
+import config from './config.js';
+import { offMenuPages, onMenuPages, server } from './index.js';
+import { emitter } from "./src/events.js";
+import projectDir from './src/get-project-dir.js';
+
+emitter.on('all-ready', async () => {
+    const allLanguages = config.CONTENT_LANGUAGES.split(',');
+    const targetDir = join(projectDir, config.BUILD_DIR);
+    rmSync(targetDir, { recursive: true });
+    const allPages = [...onMenuPages, ...offMenuPages];
+    await Promise.all(allLanguages.map(async lang => 
+            await Promise.all(allPages.map(async page => {
+                const response = await axios.get(page.url, {
+                    baseURL: `${config.PROTOCOL}://localhost:${config.PORT}/${lang}`
+                });
+                const html = response.data;
+                const filepath = join(targetDir, lang, page.url, 'index.html');
+                mkdirSync(dirname(filepath), { recursive: true });
+                writeFileSync(filepath, html);
+            }))
+        )
+    );
+    const staticDir = join(projectDir, 'static');
+    copyFolder.cp(staticDir, targetDir);
+    server.close();
+});
--- default-env.js
+++ default-env.js
@@ -5,5 +5,6 @@ export default {
     DEFAULT_LANG: 'en',
     DEFAULT_CONTENT_HEADING_LEVEL: 2,
     ALLOW_SEARCH_IN_OFF_MENU: 'true',
-    CONTENT_LANGUAGES: 'en,fr,de,tr'
+    CONTENT_LANGUAGES: 'en,fr,de,tr',
+    BUILD_DIR: './dist'
 }
