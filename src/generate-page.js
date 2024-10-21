fc245c9986e43cc4afbf7b4456022160acfdafdb
Ufuk Bakan <ubakan@altayer.com>
2024-10-21 15:10:33 +0300

    feat: backend plugins & customizable search form

--- src/generate-page.js
+++ src/generate-page.js
@@ -1,30 +1,34 @@
 import { readFileSync } from "fs";
-import { resolve } from "path";
-import generateTableOfContents from "./genereate-toc.js";
-import generateNavigation from "./generate-nav.js";
 import config from "../config.js";
-import getHeader from "./get-header.js";
-import getFooter from "./get-footer.js";
-import getLogo from "./get-logo.js";
 import dictionary from "../dictionary.js";
+import generateNavigation from "./generate-nav.js";
+import generateTableOfContents from "./genereate-toc.js";
+import getFooter from "./get-footer.js";
+import getHeader from "./get-header.js";
+import getLogo from "./get-logo.js";
+import getTemplate from "./get-template.js";
+import getSearchForm from "./get-search-form.js";
+import applyBePlugins from "./apply-be-plugins.js";
 
 export default function generatePage({ dir, content = '', meta = {}, lang = config.DEFAULT_LANG } = {}){
-    const templateHtml = readFileSync(resolve(import.meta.dirname, 'template.html'), { encoding: config.ENCODING });
+    const templateHtml = readFileSync(getTemplate(dir), { encoding: config.ENCODING });
     const htmlWithContentOnly = templateHtml.replace('%content%', content);
     const logo = getLogo(dir, lang);
     const header = getHeader(dir, lang);
     const footer = getFooter(dir, lang);
+    const searchForm = getSearchForm();
     const searchText = dictionary.search[lang];
     const toc = meta.generateTOC ? generateTableOfContents(htmlWithContentOnly, lang) : "";
     const html = htmlWithContentOnly
-        .replaceAll('%logo%', logo)
-        .replaceAll('%header%', header)
-        .replaceAll('%lang%', lang)
-        .replaceAll('%encoding%', config.ENCODING)
-        .replaceAll('%page_title%', meta.title)
-        .replaceAll('%table_of_contents%', toc)
-        .replaceAll('%search_text%', searchText)
-        .replaceAll('%nav%', generateNavigation(lang))
-        .replaceAll('%footer%', footer);
-    return html;
+        .replaceAll('%logo%', logo ?? '%logo%')
+        .replaceAll('%header%', header ?? '%header%')
+        .replaceAll('%lang%', lang ?? '%lang%')
+        .replaceAll('%encoding%', config.ENCODING ?? '%encoding%')
+        .replaceAll('%page_title%', meta.title ?? '%page_title%')
+        .replaceAll('%table_of_contents%', toc ?? '%table_of_contents%')
+        .replaceAll('%search%', searchForm ?? '%search%')
+        .replaceAll('%search_text%', searchText ?? '%search_text%')
+        .replaceAll('%nav%', generateNavigation(lang) ?? '%nav%')
+        .replaceAll('%footer%', footer) ?? '%footer%';
+    return applyBePlugins({ html, dir, lang, meta });
 }
