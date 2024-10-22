eaa92009a592f522dd5dd756d6ba927c89098649
Ufuk Bakan <ubakan@altayer.com>
2024-10-22 17:30:19 +0300

    feat: executable cli & build command

---
+++ bin/index.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+
+import assert from 'assert';
+
+async function main() {
+    // Capture command-line arguments
+    const args = process.argv.slice(2);
+
+    if (args.includes('--build')) {
+        const build = (await import('../build.js')).default;
+        await build();
+    } else {
+        const { server } = await import('../index.js');
+        assert(server, 'Server is not defined');
+    }
+}
+
+main();
--- build.js
+++ build.js
@@ -7,24 +7,31 @@ import { offMenuPages, onMenuPages, server } from './index.js';
 import { emitter } from "./src/events.js";
 import projectDir from './src/get-project-dir.js';
 
-emitter.on('all-ready', async () => {
-    const allLanguages = config.CONTENT_LANGUAGES.split(',');
-    const targetDir = join(projectDir, config.BUILD_DIR);
-    rmSync(targetDir, { recursive: true });
-    const allPages = [...onMenuPages, ...offMenuPages];
-    await Promise.all(allLanguages.map(async lang => 
-            await Promise.all(allPages.map(async page => {
-                const response = await axios.get(page.url, {
-                    baseURL: `${config.PROTOCOL}://localhost:${config.PORT}/${lang}`
-                });
-                const html = response.data;
-                const filepath = join(targetDir, lang, page.url, 'index.html');
-                mkdirSync(dirname(filepath), { recursive: true });
-                writeFileSync(filepath, html);
-            }))
-        )
-    );
-    const staticDir = join(projectDir, 'static');
-    copyFolder.cp(staticDir, targetDir);
-    server.close();
-});
+export default function build(){
+    return new Promise((resolve, reject) => {
+        emitter.on('all-ready', async () => {
+            try{
+                const allLanguages = config.CONTENT_LANGUAGES.split(',');
+                const targetDir = join(projectDir, config.BUILD_DIR);
+                rmSync(targetDir, { recursive: true });
+                const allPages = [...onMenuPages, ...offMenuPages];
+                await Promise.all(allLanguages.map(async lang => 
+                        await Promise.all(allPages.map(async page => {
+                            const response = await axios.get(page.url, {
+                                baseURL: `${config.PROTOCOL}://localhost:${config.PORT}/${lang}`
+                            });
+                            const html = response.data;
+                            const filepath = join(targetDir, lang, page.url, 'index.html');
+                            mkdirSync(dirname(filepath), { recursive: true });
+                            writeFileSync(filepath, html);
+                        }))
+                    )
+                );
+                const staticDir = join(projectDir, 'static');
+                copyFolder.cp(staticDir, targetDir);
+                server.close();
+                resolve(true);
+            } catch(e){ reject(e) }
+        });
+    });
+}
