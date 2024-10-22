89f9ec5aeb8dc81164474eb0b0dd6512a29bcd7a
Ufuk Bakan <ubakan@altayer.com>
2024-10-22 17:11:45 +0300

    feat: configure eslint for both envrionments

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,18 @@ import pluginJs from "@eslint/js";
 
 
 export default [
-  {languageOptions: { globals: globals.browser }},
   pluginJs.configs.recommended,
+  {
+    ignores: [
+      "dist/*",
+      "static/js/fuse_7_0_0.js"
+    ],
+  },
+  {
+    languageOptions: { globals: globals.node },
+  },
+  {
+    files: ["static/**/*.[jt]s?(x)", "src/plugins/frontend/**/*.[jt]s?(x)"],
+    languageOptions: { globals: globals.browser },
+  },
 ];
