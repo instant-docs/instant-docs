89bf3f5c168950a9e924a90525b07302a53e2832
Ufuk Bakan <ubakan@altayer.com>
2024-11-06 09:43:30 +0300

    fix: typos

--- src/generate-page.js
+++ src/generate-page.js
@@ -11,35 +11,33 @@ import getSearchForm from './get-search-form.js';
 import applyBePlugins from './apply-be-plugins.js';
 import putVariables from './put-variables-to-text.js';
 import getLinkFor from './get-link-for.js';
-import { maskVariables, unmaskVariables } from './mask-variables.js';
 
 const searchForm = getSearchForm();
 
 export default function generatePage({ dir, content = '', meta = {}, lang = config.DEFAULT_LANG } = {}) {
   let html = readFileSync(getTemplate(dir), { encoding: config.ENCODING });
-  if (meta.replacePlacholders) {
+  if (meta.replacePlaceholders) {
     html = html.replaceAll('%content%', content);
   }
   const varMap = {
-    '%logo%': getLogo(dir, lang) || getLogo(dir, config.DEFAULT_LANG),
+    '%logo%': getLogo(dir, lang),
     '%lang%': lang,
-    '%header%': getHeader(dir, lang) || getHeader(dir, config.DEFAULT_LANG),
-    '%footer%': getFooter(dir, lang) || getFooter(dir, config.DEFAULT_LANG),
+    '%header%': getHeader(dir, lang),
+    '%footer%': getFooter(dir, lang),
     '%search%': searchForm,
-    '%search_text%': dictionary.search[lang] || dictionary.searchp[config.DEFAULT_LANG] || 'Search',
+    '%search_text%': dictionary.search[lang] || 'Search',
     '%encoding%': config.ENCODING,
     '%home_link%': getLinkFor({ lang, page: { url: '/', metas: {} } }),
     '%nav%': generateNavigation(lang),
-    '%content%': meta.replacePlaceholders ? content : maskVariables(content),
   };
   html = putVariables({ text: html, object: varMap, placeholderAlreadyWrapped: true });
   html = putVariables({ text: html, object: meta, prefix: 'meta_' });
-
-  const toc = meta.generateTOC ? generateTableOfContents(html, lang) : '';
+  const toc = meta.generateTOC ? generateTableOfContents(content, lang) : '';
   html = html.replaceAll('%table_of_contents%', toc ?? '%table_of_contents%');
 
   if (!meta.replacePlaceholders) {
-    html = html.replaceAll(content, unmaskVariables(content));
+    html = html.replaceAll('%content%', content);
   }
+
   return applyBePlugins({ html, dir, lang, meta });
 }
