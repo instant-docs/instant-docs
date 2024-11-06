eeb72689437a378d8246859504e78a2021993ff1
Ufuk Bakan <ubakan@altayer.com>
2024-11-06 09:42:28 +0300

    feat: variable placeholders as snake case

--- src/put-variables-to-text.js
+++ src/put-variables-to-text.js
@@ -1,5 +1,8 @@
-export default function putVariables({ text, object, prefix = '', suffix = '', placeholderAlreadyWrapped = false }) {
+import { toSnakeCase } from './to-snakecase.js';
+
+export default function putVariables({ text, object, prefix = '', suffix = '', placeholderAlreadyWrapped = false, convertToSnakeCase = true }) {
   Object.keys(object || {}).forEach((key) => {
+    key = convertToSnakeCase ? toSnakeCase(key) : key;
     const placeholderSymbol = placeholderAlreadyWrapped ? '' : '%';
     const placeholder = `${placeholderSymbol}${prefix}${key}${suffix}${placeholderSymbol}`;
     text = text.replaceAll(placeholder, object[key]);
